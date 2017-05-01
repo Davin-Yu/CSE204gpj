eee0a233c9227b558ab90a3f607055f9aaa9beec
Davin Difeng Yu <difeng.yu@outlook.com>
2017-05-01 20:43:37 +0800

    Smaller

--- RSA/Algorithm.java
+++ RSA/Algorithm.java
@@ -7,7 +7,7 @@ import java.math.*;
 import java.util.Random;
 
 public class Algorithm {
-	
+
 	private static final int maxBitLength = 100;
 	private static final int certainty = 1000;
 	private static BigInteger plaintext;
@@ -19,7 +19,7 @@ public class Algorithm {
 	private static BigInteger fei;
 	private static BigInteger e;
 	private static BigInteger d;
-	
+
 	private static void print() {
 		System.out.println("plaintext: " + plaintext);
 		System.out.println("p: " + p + " q: " + q);
@@ -28,19 +28,19 @@ public class Algorithm {
 		System.out.println("ciphertext: " + ciphertext);
 		System.out.println("Tranfer back: " + backtext);
 	}
-	
+
 	private static BigInteger getRandomBigInteger(BigInteger upperBound) {
-	    BigInteger result;
-	    do {
-	    	result = new BigInteger(upperBound.bitLength(), new Random());
-	    } while (result.compareTo(upperBound) >= 0);
-	    return result;
+		BigInteger result;
+		do {
+			result = new BigInteger(upperBound.bitLength(), new Random());
+		} while (result.compareTo(upperBound) >= 0);
+		return result;
 	}
-	
+
 	private static BigInteger getPrime() {
 		return new BigInteger(maxBitLength, certainty, new Random());
 	}
-	
+
 	private static BigInteger getE(BigInteger fei) {
 		BigInteger e;
 		do {
@@ -48,15 +48,15 @@ public class Algorithm {
 		} while (! (e.gcd(fei).compareTo(new BigInteger("1")) == 0) );
 		return e;
 	}
-	
+
 	private static BigInteger encryption(BigInteger M, BigInteger e, BigInteger n) {
 		return M.modPow(e, n);
 	}
-	
+
 	private static BigInteger decryption(BigInteger C, BigInteger d, BigInteger n) {
 		return C.modPow(d, n);
 	}
-	
+
 	public static void main(String args[]) throws IOException {
 		System.out.print("Please input a number (max bit length " + maxBitLength + "): ");
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
